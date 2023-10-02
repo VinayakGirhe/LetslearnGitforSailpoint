5c77e38ec982e5684dcfa4ea0580db62fce0dd85
Vinayak Dinesh Girhe <vinayakgirhe1@gmail.com>
2023-10-02 23:53:37 +0530

    adding message in Add.java

--- Add.java
+++ Add.java
@@ -2,6 +2,6 @@ public class Add {
     public static void main(String[] args) {
         double x = 8;
         double y = 9;
-        System.out.println(x+y);
+        System.out.print(x+y);
     }
 }
